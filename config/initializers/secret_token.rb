31a884a72ef2008863949708a98ec1848062d0dc
dule orlovic <orlovics@eunet.rs>
2012-02-10 12:30:47 +0100

    inicijalno

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Proba::Application.config.secret_token = 'ea4526cdf500bc72cc68a0637be383d56fac38d7e3a8de98c28db3b9d1280df850bd9ee338edbd85d14f373dbc1a6cbab4fc46a3213a28ffcaa0553e2d380b2b'
